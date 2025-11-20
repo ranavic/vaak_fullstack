fb2ec506d143b6bf4bf41f4c331239fdb0b19eba
ranavic <ambasthasubham@gmail.com>
2025-11-21 01:55:47 +0530

    vite.config changes

--- vaak-frontend/vite.config.js
+++ vaak-frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/vaak_fullstack/',
-});
+  base: command === 'serve' ? '/' : '/vaak_fullstack/',
+}));
