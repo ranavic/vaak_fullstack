83c78d3472d9fd7dc063efe8e09c5ea2364b4801
ranavic <ambasthasubham@gmail.com>
2026-03-10 00:44:41 +0530

    CORS mismatch bug

--- vaak-backend/app/main.py
+++ vaak-backend/app/main.py
@@ -8,7 +8,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://vaakfrontend.vercel.app",
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "http://127.0.0.1:5173"
     ],
     allow_credentials=True,
     allow_methods=["*"],
